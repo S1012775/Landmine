e02b306bc2ebf888570a8a4e78143453961b8c08
yuchen <as22877566@yahoo.com.tw>
2016-08-22 08:47:02 +0000

    進階題

--- Landmine2.php
+++ Landmine2.php
@@ -1,32 +1,40 @@
 <?php
-//隨機產生40個從1 ~ 100個炸彈
-for($i = 0; $i < 40; $i ++) {  //產生40個
-    $bomb = rand(1,100);  //產生1~100的亂數
+$time1 = microtime(true);
+$row = 60;
+$col = 50;
+
+//隨機產生40個從1 ~ 3000的炸彈
+for($i = 0; $i < 1200; $i ++) {  //產生120個
+    $bomb = rand(1,3000);  //產生1~3000的亂數
     for($j = 0; $j <= $i; $j ++) {  //檢查重覆
         if ($bomb == $bombNum[$j]){
-            $bomb = rand(1,100);  //如果重覆，重新產生亂數
+            $bomb = rand(1,3000);  //如果重覆，重新產生亂數
             $j = 0;
         }
     }
     $bombNum[$i] = $bomb;  //寫入陣列
 }
+
 //產生陣列
 $frame = [];
-for ($i = 0; $i < 100; $i ++) {
+for ($i = 0; $i < 3000; $i ++) {
     $frame[] = "0";
 }
+
 //放入炸彈
-for ($i = 0; $i < 40; $i ++) {
+for ($i = 0; $i < 1200; $i ++) {
     $where = $bombNum[$i];
         if ($frame[$where] == "0") {
             $frame[$where] = "M";
     }
 }
+
 //轉成二微陣列
-$landmine = array_chunk($frame,10);
+$landmine = array_chunk($frame, $row);
+
 //判斷上下左右
-for ($m = 0; $m < 10; $m ++){
-    for ($n = 0; $n < 10; $n ++){
+for ($m = 0; $m < $col; $m ++){
+    for ($n = 0; $n < $row; $n ++){
         if ($landmine[$m][$n] === "M"){
             if ($landmine[$m][$n - 1] !== "M"){  //上
                 $landmine[$m][$n - 1] ++;
@@ -55,12 +63,17 @@ for ($m = 0; $m < 10; $m ++){
         }
     }
 }
+
 //輸出
-for ($m = 0; $m < 10; $m ++){
-    for ($n = 0; $n < 10; $n ++){
+for ($m = 0; $m < $col; $m ++){
+    for ($n = 0; $n < $row; $n ++){
         echo $landmine[$m][$n];
     }
-    if ($m < 9) {
+    if ($m < $col - 1) {
         echo "N";
     }
 }
+
+echo "<hr>";
+$time2 = microtime(true);
+echo $time2 - $time1;
